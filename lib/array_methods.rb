ab70d08fd5b99706b9f3ecf8369c68c070ba8ed0
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-28 02:07:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,17 +3,22 @@ famous_cats = ["Felix", "Tom", "Garfield"]
 famous_cats.include? ("Nala")
 end
 
+def
 famous_cats.sort 
 end
 
+def
 famous_cats.reverse
 end
 
+def
 famous_cats.first 
 end
 
+def
 famous_cats.last 
 end 
 
+def
 famous_cats.size 
 end 
